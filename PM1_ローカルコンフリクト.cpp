4726dfffe0d92841cc16de7412371133ff24d6f6
YutoIsogaiGithub <115062580+JMacfield@users.noreply.github.com>
2022-11-09 15:48:59 +0900

    Update PM1_ローカルコンフリクト.cpp
    リモートリポジトリでコンフリクト変更

--- PM1_ローカルコンフリクト.cpp
+++ PM1_ローカルコンフリクト.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    std::cout << "Remote Clone Changed\n";
+    std::cout << "Github Clone Changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
