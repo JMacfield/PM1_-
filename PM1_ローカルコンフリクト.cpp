e2249090223d57345bb56e5aa58dfcc5c4ea6023
k022g0135@g.neec.ac.jp <k022g0135@g.neec.ac.jp>
2022-11-09 15:46:33 +0900

    ローカルリポジトリでコンフリクト変更

--- PM1_ローカルコンフリクト.cpp
+++ PM1_ローカルコンフリクト.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    std::cout << "Remote Changed\n";
+    std::cout << "Clone Changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
